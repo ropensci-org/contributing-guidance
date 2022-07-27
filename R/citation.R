1716021afdb6443308bbaa3dd800500772291188
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-07-27 08:53:52 +0200

    more quarto

---
+++ R/citation.R
@@ -0,0 +1,7 @@
+#' Citation for the contributing guide
+#'
+#' @return Character
+#' @export
+citation <- function() {
+  "Stefanie Butland & Steffi LaZerte. (2020). rOpenSci Community Contributing Guide (Version v0.1.0). Zenodo. http://doi.org/10.5281/zenodo.4000532"
+}
