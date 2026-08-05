a7a28a9339efdc58351625e61ccd581141b46599
Steffi LaZerte <steffi@steffi.ca>
2026-08-05 11:51:17 -0500

    Fix zenodo badges (part of #115)

--- R/citation.R
+++ R/citation.R
@@ -3,5 +3,5 @@
 #' @return Character
 #' @export
 citation <- function() {
-  "Stefanie Butland & Steffi LaZerte. (2020). rOpenSci Community Contributing Guide (Version v0.1.0). Zenodo. http://doi.org/10.5281/zenodo.4000532"
+  "Stefanie Butland & Steffi LaZerte. (2020). rOpenSci Community Contributing Guide (Version v0.1.0). Zenodo. <https://doi.org/10.5281/zenodo.4000531>"
 }
