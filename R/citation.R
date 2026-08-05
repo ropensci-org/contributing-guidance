2926d68b56344e5a7c95e7eeacb99898aa96fd27
Steffi LaZerte <steffi@steffi.ca>
2026-08-05 12:12:19 -0500

    Update authors and build citation from DESCRIPTION (#89 and #115)

--- R/citation.R
+++ R/citation.R
@@ -3,5 +3,26 @@
 #' @return Character
 #' @export
 citation <- function() {
-  "Stefanie Butland & Steffi LaZerte. (2020). rOpenSci Community Contributing Guide (Version v0.1.0). Zenodo. <https://doi.org/10.5281/zenodo.4000531>"
+  v <- packageVersion("contributingguidance")
+  year <- packageDescription("contributingguidance", fields = "Year")
+  authors <- packageDescription("contributingguidance", fields = "Authors@R") |>
+    parse(text = _) |>
+    eval() |>
+    format(include = c("given", "family"))
+
+  if (length(authors) == 2) {
+    authors_fmt <- paste0(authors, collapse = " and ")
+  } else {
+    authors_fmt <- paste0(authors[-length(authors)], collapse = ", ")
+    authors_fmt <- paste0(authors_fmt, ", and ", authors[length(authors)])
+  }
+
+  paste0(
+    authors_fmt,
+    ". (",
+    year,
+    "). rOpenSci Community Contributing Guide (Version v",
+    v,
+    "). Zenodo. <https://doi.org/10.5281/zenodo.4000531>"
+  )
 }
